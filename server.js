02f74d1769d73980a03277410e14da27b509b06d
rte490 <rte490@gmail.com>
2015-11-06 18:45:10 -0800

    added comments to server.js and index.ejs

--- server.js
+++ server.js
@@ -18,19 +18,31 @@ var users = {};
 // socket functionality
 io.sockets.on('connection', function(socket) {
   // set up socket listeners for a particular socket when a connection is made
-  socket.on('newUser', function(data) {
+  // listening for event 'newUser'
+  socket.on('newUser', function(data) { 
+    // setting 'users[socket.id]' to equal to name provided by client/user 
     users[socket.id] = data.name;
+    // emitting 'initialMessages' event and passing data in messages variable to client side
     socket.emit('initialMessages', {messages: messages});
+    // setting variable message to equal name provided by client/user plus a string 
     var message = data.name + ' has joined the chat!';
+    // pushing variable message to array 'messages'
     messages.push(message);
+    // emitting event 'newMessage' to everyone except user who created new message to inform other users in the room that a new user has joined the room
     socket.broadcast.emit('newMessage', {message: message});
   });
+  // listening for event 'messageSubmit'
   socket.on('messageSubmit', function(data) {
+    // push message data to array 'messages'
     messages.push(data.message);
+    // emit event 'newMessage' to everyone and pass data message to client side
     io.emit('newMessage', {message: data.message});
   });
+  // listening for event 'disconnect', event occurs when user disconnects from socket connection
   socket.on('disconnect', function() {
+    // setting message equal to user name (provided from user on client side) plus string to indicate user has disconnected
     var message = users[socket.id] + ' has disconnected!';
+    // emitting event 'newMessage' and passing data message (variable) to client side
     socket.broadcast.emit('newMessage', {message: message});
   });
 })
